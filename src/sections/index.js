30b410754d700709b7bee753b9c0f9fbfa745ff0
Clifford Kovulo <cliffkkilonzo@gmail.com>
2023-09-27 15:57:11 +0300

    nike commit

---
+++ src/sections/index.js
@@ -0,0 +1,34 @@
+import Footer from "./Footer";
+import Hero from './Hero';
+import Nav from './Nav';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import CustomerReviews from './CustomerReviews';
+
+const sections = {
+    Footer,
+    Hero,
+    Nav,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews
+};
+
+export default sections;
+export {
+    Footer,
+    Hero,
+    Nav,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews
+};
